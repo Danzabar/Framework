b6225c886a51039d534609e101e8970d0daf6932
Dan Cox <danzabian@gmail.com>
2015-03-11 19:44:11 +0000

    Adding test for exception

--- tests/DI/DITest.php
+++ tests/DI/DITest.php
@@ -26,6 +26,12 @@ class DITest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals('foo', $di->getDirectory());
 	}
 
+	/**
+	 * Test Building DI
+	 *
+	 * @return void
+	 * @author Dan Cox
+	 */		
 	public function test_Build()
 	{
 		$di = new DI;
@@ -34,6 +40,20 @@ class DITest extends \PHPUnit_Framework_TestCase
 
 		$this->assertInstanceOf('Wasp\Exceptions\DI\InvalidServiceDirectory', $di->get('excep'));
 		$this->assertEquals('value', $di->param('test'));
+	}
+
+	/**
+	 * Test that an exception is thrown if trying to build without a directory set
+	 *
+	 * @return void
+	 * @author Dan Cox
+	 */
+	public function test_buildWithoutDirectory()
+	{
+		$this->setExpectedException('Wasp\Exceptions\DI\InvalidServiceDirectory');
+
+		$di = new DI;
+		$di->build();
 	}
 
 
