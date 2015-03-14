2463fb32c255c3f2d5a8ef4b889dbf339cebc490
Dan Cox <danzabian@gmail.com>
2015-03-14 12:38:18 +0000

    Adding invalid connection exception for database classes

---
+++ src/Exceptions/Database/InvalidConnection.php
@@ -0,0 +1,43 @@
+<?php namespace Wasp\Exceptions\Database;
+
+/**
+ * Exception class for invalid connections
+ *
+ * @package Wasp
+ * @subpackage Exceptions\Database
+ * @author Dan Cox
+ */
+class InvalidConnection extends \Exception
+{
+
+	/**
+	 * The name of the connection attempted
+	 *
+	 * @var string
+	 */
+	protected $connection;
+
+	/**
+	 * Fire Exception
+	 *
+	 * @author Dan Cox
+	 */
+	public function __construct($connection, $code = 0, \Exception $previous = NULL)
+	{
+		$this->connection = $connection;
+
+		parent::__construct("An attempt was made to use an invalid connection: $connection", $code, $previous);
+	}
+
+	/**
+	 * Returns the Connection Var
+	 *
+	 * @return String
+	 * @author Dan Cox
+	 */
+	public function getConnection()
+	{
+		return $this->connection;
+	}
+	
+} // END class InvalidConnection extends \Exception
---
+++ tests/Exceptions/Database/InvalidConnectionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Wasp\Exceptions\Database\InvalidConnection;
+
+/**
+ * Test case for the Invalid Connection exception
+ *
+ * @package Wasp
+ * @subpackage Tests\Exceptions
+ * @author Dan Cox
+ */
+class InvalidConnectionTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Test firing the exception
+	 *
+	 * @return void
+	 * @author Dan Cox
+	 */
+	public function test_fire()
+	{
+		try {
+			throw new InvalidConnection("Test");
+		} catch (\Exception $e) {
+			$this->assertEquals("Test", $e->getConnection());
+		}
+	}
+
+} // END class InvalidConnectionTest extends \PHPUnit_Framework_TestCase
