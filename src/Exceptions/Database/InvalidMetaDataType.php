290d7a27cddbd6149b202037d2589e83bef5e9c2
Dan Cox <danzabian@gmail.com>
2015-03-14 14:19:46 +0000

    Exception class for selecting invalid meta data type configurations on a connection

---
+++ src/Exceptions/Database/InvalidMetaDataType.php
@@ -0,0 +1,65 @@
+<?php namespace Wasp\Exceptions\Database;
+
+
+/**
+ * Exception class for the meta data type property in connections
+ *
+ * @package Wasp
+ * @subpackage Exceptions\Database
+ * @author Dan Cox
+ */
+class InvalidMetaDataType extends \Exception
+{
+	
+	/**
+	 * The metadata type
+	 *
+	 * @var string
+	 */
+	protected $metadata;
+
+	/**
+	 * The metadata types allowed
+	 *
+	 * @var Array
+	 */
+	protected $typesAllowed;
+
+	/**
+	 * Fire exception
+	 *
+	 * @author Dan Cox
+	 */
+	public function __construct($metadata, Array $typesAllowed, $code = 0, \Exception $previous = NULL)
+	{
+		$this->metadata = $metadata;
+		$this->typesAllowed = $typesAllowed;
+
+		parent::__construct("Invalid metadata type $metadata, allowed metadata types: ". join(',', $typesAllowed), $code, $previous);
+	}
+
+	/**
+	 * Returns the metadata string
+	 *
+	 * @return String
+	 * @author Dan Cox
+	 */
+	public function getMetadata()
+	{
+		return $this->metadata;
+	}
+
+	/**
+	 * Returns the allowed metadata
+	 *
+	 * @return Array
+	 * @author Dan Cox
+	 */
+	public function getAllowedTypes()
+	{
+		return $this->typesAllowed;
+	}
+
+} // END class InvalidMetaDataType extends \Exception
+
+
---
+++ tests/Exceptions/Database/InvalidMetaDataTypeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Wasp\Exceptions\Database\InvalidMetaDataType;
+
+/**
+ * Test for the Exception InvalidMetaDataType
+ *
+ * @package Wasp
+ * @subpackage Tests\Exception
+ * @author Dan Cox
+ */
+class InvalidMetaDataTest extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * Just fire the exception
+	 *
+	 * @return void
+	 * @author Dan Cox
+	 */
+	public function test_fire()
+	{
+		try {
+			throw new InvalidMetaDataType("Test", ["Annotation", "None"]);
+		} catch (\Exception $e) {
+			$this->assertEquals("Test", $e->getMetadata());
+			$this->assertEquals(["Annotation", "None"], $e->getAllowedTypes());
+		}
+	}
+
+} // END class InvalidMetaDataTest extends \PHPUnit_Framework_TestCase
