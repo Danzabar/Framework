5f9911fbc470841aa5201f8e1a073d312380cb11
Dan Cox <danzabian@gmail.com>
2015-03-14 16:54:14 +0000

    Adding exception for invalid connection types

---
+++ src/Exceptions/Database/InvalidConnectionType.php
@@ -0,0 +1,62 @@
+<?php namespace Wasp\Exceptions\Database;
+
+/**
+ * Exception class for an invalid connection type
+ *
+ * @package Wasp
+ * @subpackage Exceptions\Database
+ * @author Dan Cox
+ */
+class InvalidConnectionType extends \Exception
+{
+	
+	/**
+	 * The Attempted type
+	 *
+	 * @var string
+	 */
+	protected $type;
+
+	/**
+	 * The Allowed types
+	 *
+	 * @var Array
+	 */
+	protected $allowed;
+
+	/**
+	 * Fire exception
+	 *
+	 * @author Dan Cox
+	 */
+	public function __construct($type, Array $allowed, $code = 0, \Exception $previous = NULL)
+	{
+		$this->type = $type;
+		$this->allowed = $allowed;
+
+		parent::__construct("An invalid connection type was used $type, connections can be one of: ". join(',', array_keys($allowed)), $code, $previous);
+	}
+
+	/**
+	 * Returns the type 
+	 *
+	 * @return string
+	 * @author Dan Cox
+	 */
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	/**
+	 * Returns the allowed type
+	 *
+	 * @return Array
+	 * @author Dan Cox
+	 */
+	public function getAllowed()
+	{
+		return $this->allowed;
+	}
+
+} // END class InvalidConnectionType extends \Exception
---
+++ tests/Exceptions/Database/InvalidConnectionTypeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Wasp\Exceptions\Database\InvalidConnectionType;
+
+/**
+ * Test case for the Invalid connection type exception class
+ *
+ * @package Wasp
+ * @subpackage Test\Exceptions
+ * @author Dan Cox
+ */
+class InvalidConnectionTypeTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Fire exception
+	 *
+	 * @return void
+	 * @author Dan Cox
+	 */
+	public function test_fire()
+	{
+		try {
+			throw new InvalidConnectionType("Test", ["Array" => "convertFromArray"]);
+		} catch (\Exception $e) {
+			$this->assertEquals("Test", $e->getType());
+			$this->assertEquals(["Array" => "convertFromArray"], $e->getAllowed());
+		}
+	}
+		
+} // END class InvalidConnectionTypeTest extends \PHPUnit_Framework_TestCase
