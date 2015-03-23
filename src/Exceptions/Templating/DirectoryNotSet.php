c07b5136a1732f266405ce9f5972efe1a4f7720a
Dan Cox <danzabian@gmail.com>
2015-03-23 08:09:05 +0000

    Adding exception class for templating

---
+++ src/Exceptions/Templating/DirectoryNotSet.php
@@ -0,0 +1,27 @@
+<?php namespace Wasp\Exceptions\Templating;
+
+/**
+ * Exception class for directory not set
+ *
+ * @package Wasp
+ * @subpackage Exceptions
+ * @author Dan Cox
+ */
+class DirectoryNotSet extends \Exception
+{
+
+	/**
+	 * Fire exception
+	 *
+	 * @author Dan Cox
+	 */
+	public function __construct($code = 0, \Exception $previous = NULL)
+	{
+		parent::__construct(
+			"Attempted to create an instance of Delegation Engine without a directory set",
+			$code,
+			$previous
+		);
+	}
+	
+} // END class DirectoryNotSet extends \Exception
---
+++ tests/Exceptions/Templating/DirectoryNotSetTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Wasp\Exceptions\Templating\DirectoryNotSet;
+
+
+/**
+ * Test case for the directory not set exception
+ *
+ * @package Wasp
+ * @subpackage Tests\Exceptions
+ * @author Dan Cox
+ */
+class DirectoryNotSetTest extends \PHPUnit_Framework_TestCase
+{
+	
+	/**
+	 * Test fire exception
+	 *
+	 * @author Dan Cox
+	 */
+	public function test_fire()
+	{
+		try {
+			throw new DirectoryNotSet;
+		} catch (\Exception $e) {
+			$this->assertContains("Attempted to create an instance of Delegation Engine", $e->getMessage());
+		}
+	}
+
+} // END class DirectoryNotSetTest extends \PHPUnit_Framework_TestCase
