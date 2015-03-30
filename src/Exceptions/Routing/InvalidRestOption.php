d2ae78a73bfb75f3b588c92d095865ad9ef52aac
Dan Cox <danzabian@gmail.com>
2015-03-30 07:14:27 +0000

    Adding exception class for routing

---
+++ src/Exceptions/Routing/InvalidRestOption.php
@@ -0,0 +1,66 @@
+<?php namespace Wasp\Exceptions\Routing;
+
+/**
+ * Exception class for a wrong rest option selection
+ *
+ * @package Wasp
+ * @subpackage Exceptions\Routing
+ * @author Dan Cox
+ */
+class InvalidRestOption extends \Exception
+{
+	
+	/**
+	 * The offending option
+	 *
+	 * @var string
+	 */
+	protected $option;
+
+	/**
+	 * The correct list of options
+	 *
+	 * @var Array
+	 */
+	protected $list;
+
+	/**
+	 * Fire exception
+	 *
+	 * @param String $option
+	 * @param Array $list
+	 * @param Integer $code
+	 * @param \Exception $exception
+	 * @author Dan Cox
+	 */
+	public function __construct($option, $list, $code = 0, \Exception $previous = NULL)
+	{
+		$this->option = $option;
+		$this->list = $list;
+
+		parent::__construct("The rest route option $option is invalid.", $code, $previous);
+	}
+
+	/**
+	 * Returns the option
+	 *
+	 * @return String
+	 * @author Dan Cox
+	 */
+	public function getOption()
+	{	
+		return $this->option;
+	}
+
+	/**
+	 * Returns the list
+	 *
+	 * @return Array
+	 * @author Dan Cox
+	 */
+	public function getList()
+	{	
+		return $this->list;
+	}
+
+} // END class InvalidRestOption extends \Exception
---
+++ tests/Exceptions/Routing/InvalidRestOptionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Wasp\Exceptions\Routing\InvalidRestOption;
+
+/**
+ * Test case for the Invalid rest option exception
+ *
+ * @package Wasp
+ * @subpackage Tests\Exceptions
+ * @author Dan Cox
+ */
+class InvalidRestOptionTest extends \PHPUnit_Framework_TestCase
+{
+	
+	/**
+	 * Fire exception 
+	 *
+	 * @return void
+	 * @author Dan Cox
+	 */
+	public function test_fireException()
+	{
+		try {
+			throw new InvalidRestOption('test', ['foo', 'bar']);
+		} catch (\Exception $e) {
+			$this->assertEquals('test', $e->getOption());
+			$this->assertEquals(['foo', 'bar'], $e->getList());
+		}
+	}
+
+} // END class InvalidRestOptionTest extends \PHPUnit_Framework_TestCase
