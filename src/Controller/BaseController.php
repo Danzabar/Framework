b81d84929fc0e851a1ea6e4748474706da1ff48a
Dan Cox <danzabian@gmail.com>
2015-05-11 17:44:39 +0000

    Added a get method for services that cant be fetched via magic getter

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -20,7 +20,6 @@ class BaseController
 	/**
 	 * Load the DI
 	 *
-	 * @return void
 	 * @author Dan Cox
 	 */
 	public function __construct($DI)
@@ -31,12 +30,27 @@ class BaseController
 	/**
 	 * Magic getter for accessing services from the DI
 	 *
+	 * @param String $key
 	 * @return Object
 	 * @author Dan Cox
 	 */
 	public function __get($key)
 	{
-		return $this->DI->get($key);
+		return $this->get($key);
+	}
+
+	/**
+	 * Gets a service from the DependencyInjector
+	 * This function exists for those services which have special characters and
+	 * cant be fetched via the magic method
+	 *
+	 * @param String $service
+	 * @return Object
+	 * @author Dan Cox
+	 */
+	public function get($service)
+	{
+		return $this->DI->get($service);
 	}
 
 
